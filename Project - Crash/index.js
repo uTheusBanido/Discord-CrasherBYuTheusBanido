6ac494298dcbe6c4a41db205d708dd3c1e640fb7
uTheusBanido <80640322+uTheusBanido@users.noreply.github.com>
2021-05-01 01:41:35 -0300

    Add files via upload

---
+++ Project - Crash/index.js
@@ -0,0 +1,62 @@
+const colors = require('colors')
+const readline = require('readline')
+const raid = require('./utils/raid')
+const login = require('./utils/login')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+email()
+
+async function email() {
+    process.title = `[DISCORD CRASHER] | by uTheusBanido`
+    console.clear()
+    console.log(colors.blue(`
+     ▄████▄   ██▀███   ▄▄▄        ██████  ██░ ██ ▓█████  ██▀███  
+    ▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██ ▒ ██▒
+    ▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ░ ▓██▄   ▒██▀▀██░▒███   ▓██ ░▄█ ▒
+    ▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██   ▒   ██▒░▓█ ░██ ▒▓█  ▄ ▒██▀▀█▄  
+    ▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒██████▒▒░▓█▒░██▓░▒████▒░██▓ ▒██▒
+    ░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░░ ▒░ ░░ ▒▓ ░▒▓░
+      ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░ ░ ░  ░  ░▒ ░ ▒░
+    ░          ░░   ░   ░   ▒   ░  ░  ░   ░  ░░ ░   ░     ░░   ░ 
+    ░ ░         ░           ░  ░      ░   ░  ░  ░   ░  ░   ░     
+    ░                                                            \n`))
+    rl.question(colors.green(`[1] `) + `Email: `, async (answer) => {
+        if (!answer) {
+            return email()
+        }
+        senha(answer)
+    })
+}
+
+async function senha(email) {
+    rl.question(colors.red(`[2] `) + `Senha: `, async (answer) => {
+        if (!answer) {
+            return senha(email)
+        }
+        id(email, answer)
+    })
+}
+
+async function id(email, senha) {
+    rl.question(colors.magenta(`[3] `) + `Id do Canal: `, async (answer) => {
+        if (!answer) {
+            return id(email, senha)
+        }
+        delay(email, senha, answer)
+    })
+}
+
+async function delay(email, senha, id) {
+    rl.question(colors.cyan(`[4] `) + `Delay (ms): `, async (answer) => {
+        if(!answer) {
+            return delay(email, senha, id)
+        }
+
+        login(email, senha, id, answer)
+    })
+}
